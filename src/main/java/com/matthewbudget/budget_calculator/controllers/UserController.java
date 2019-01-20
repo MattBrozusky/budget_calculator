f6334a23abfa5589070e035e13ccc0e57bbf6fb9
MattBrozusky <matthew.brozusky@gmail.com>
2019-01-20 14:36:25 -0600

    fixed errors with registration validation

--- src/main/java/com/matthewbudget/budget_calculator/controllers/UserController.java
+++ src/main/java/com/matthewbudget/budget_calculator/controllers/UserController.java
@@ -29,11 +29,18 @@ public class UserController {
 
     @PostMapping("/sign-up")
     public String saveUser(@Valid User user, Errors validation, Model model){
-        if (validation.hasErrors()) {
+        if (validation.hasErrors() || users.findByUsername(user.getUsername()) != null || users.findByEmail(user.getEmail()) != null) {
+            if (users.findByUsername(user.getUsername()) != null ){
+                model.addAttribute("usernameTaken", true);
+            }
+            if (users.findByEmail(user.getEmail()) != null){
+                model.addAttribute("emailTaken", true);
+            }
             model.addAttribute("errors", validation);
             model.addAttribute("user", user);
             return "authentication/register";
         }
+
         String hash = passwordEncoder.encode(user.getPassword());
         user.setPassword(hash);
         users.save(user);
